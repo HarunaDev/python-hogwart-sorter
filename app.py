8006cd2059689e46a3311b198a1d28fcda7f1a9a
Alvin Haruna <harunaalvin69@gmail.com>
2023-03-20 01:31:31 +0100

    added comments on functionality of app

---
+++ app.py
@@ -0,0 +1,11 @@
+# Display intro to app and how to play game
+
+# show houses in hogwarts
+
+# store houses in a list
+
+# get details from users
+
+# loop through the list and select a random house
+
+# display the details of the user and the house that the user has been selected by the hat
