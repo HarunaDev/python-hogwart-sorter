9be33c0544a890689c602d1355bec785845d6654
Alvin Haruna <harunaalvin69@gmail.com>
2023-03-20 04:30:10 +0100

    added code to display details of user and house the user has been sorted into

--- app.py
+++ app.py
@@ -5,16 +5,17 @@ print('''Welcome to Hogwarts School of Witchcraft and Wizardry
 
 Motto: Draco Dormiens Nunquam Titillandus 
 
-Enter your details to get sorted into a house in hogwarts by the sorting house.\n''')
+Enter your details to get sorted into a house in hogwarts by the sorting hat.\n''')
 
 # store houses in a list
 houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
 
 # get details from users
 name = input("Enter your full name: ")
-location = input("Enter your location")
+location = input("Enter your location: ").lower()
 
 # loop through the list and select a random house
 sorting_hat = random.choice(houses)
 
 # display the details of the user and the house that the user has been selected by the hat
+print(f"\n{name} from {location}, you have been added to {sorting_hat}!")
